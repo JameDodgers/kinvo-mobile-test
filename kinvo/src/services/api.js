62169e9eac4ec1773adf4c6abc01309bba04ee26
Pablo Henrique <pablohenrique.r.c@gmail.com>
2022-08-28 20:39:35 -0300

    Updates API base URL

--- kinvo/src/services/api.js
+++ kinvo/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://d68b5a2f-8234-4863-9c81-7c8a95dff8eb.mock.pstmn.io/',
+  baseURL: "https://6266f62263e0f382568936e4.mockapi.io/",
 });
 
 export default axiosInstance;
